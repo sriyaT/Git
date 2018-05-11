c39b69de14bea3076c1c410575b65bf9e6db578d
sriyaT <36224422+sriyaT@users.noreply.github.com>
2018-05-11 17:47:10 +0530

    Its Changed

--- Git/src/D1.java
+++ Git/src/D1.java
@@ -3,7 +3,8 @@ public class D1 {
 
 	public static void main(String[] args) {
 		System.out.println("hi all");
-		System.out.println("Sriya here");
+		System.out.println("Sriya here");\
+		System.out.println("ajdghj");
 	}
 
 }
